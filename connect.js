894ad045bb58706cf26d3a0fbc865b1171472fff
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2022-12-15 22:44:31 -0500

    modified:   connect.js modified:   js/registro.js

--- connect.js
+++ connect.js
@@ -1,3 +1,4 @@
+//Utilizando Visual Code creamos el archivo connect.js y agregamos el siguiente código:
 var express = require("express");
 var mysql = require("mysql");
 var app = express();
@@ -11,34 +12,47 @@ var conexion = mysql.createConnection({
     user: "christian",
     password: "2002",
     database: "dbFormulario",
-});
-conexion.connect(function (error) {
+  });
+//Para verificar si la conexión se ha realizado
+//ingresamos las siguientes líneas de código en el archivo connect.js
+  conexion.connect(function (error) {
     if (error) {
-        throw error;
+      throw error;
     } else {
-        console.log("Conexión exitosa");
+      console.log("Conexión exitosa");
     }
-});
+  });
+
+//Ahora procedemos a configurar el puerto de salida
+//de la aplicación con el siguiente 
+//código en el mismo archivo connect.js
 const puerto = process.env.PUERTO || 3000;
 
 app.listen(puerto, function () {
-    console.log("Servidor funcionando en puerto: " + puerto);
+  console.log("Servidor funcionando en puerto: " + puerto);
 });
+
+//Agregamos las siguientes líneas de código 
+//JavaScript  en el mismo archivo ‘connect.js’ 
+//para permitir la inserción de registros en la tabla.
+
 app.post("/api/pedido", (req, res) => {
-    let data = {
-        userped: req.body.USERPED,
-        emausped: req.body.EMAUSPED,
-        celusped: req.body.CELUSPED,
-        foodped: req.body.FOODPED,
-        msgped: req.body.MSGPED
-    };
-    let sql = "INSERT INTO pedido SET ?";
-    conexion.query(sql, data, function (error, results) {
-        if (error) {
-            throw error;
-        } else {
-            console.log(data);
-            res.send(data);
-        }
-    });
-});
+	let data = {
+    	userped: req.body.USERPED,
+    	emauser: req.body.EMAUSER,
+    	celuuser: req.body.CELUUSER,
+    	menped: req.body.MENPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO pedido SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
+
+
--- js/registro.js
+++ js/registro.js
@@ -1,10 +1,11 @@
+
 const url = "http://localhost:3000/api/pedido/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
-const emausped = document.getElementById("EMAUSPED");
-const celusped = document.getElementById("CELUSPED");
-const foodped = document.getElementById("FOODPED");
+const emauser = document.getElementById("EMAUSER");
+const celuuser = document.getElementById("CELUUSER");
+const menped = document.getElementById("MENPED");
 const msgped = document.getElementById("MSGPED");
 var opcion = '';
 
@@ -17,7 +18,7 @@ formArticulo.addEventListener('submit',
     (e) => {
         e.preventDefault();
         if (opcion == 'crear') {
-            if (USERPED.value == "" || EMAUSPED.value == "" || CELUSPED.value == "" || FOODPED.value == "" || MSGPED.value == "") {
+            if (USERPED.value == "" || EMAUSER.value == "" || CELUUSER.value == "" || MENPED.value == "" || MSGPED.value == "") {
                 alert("Asegúrese de que todos los campos estén completos");
                 return false;
             } else {
@@ -32,9 +33,9 @@ formArticulo.addEventListener('submit',
                         body: JSON.stringify(
                             {
                                 USERPED: USERPED.value,
-                                EMAUSPED: EMAUSPED.value,
-                                CELUSPED: CELUSPED.value,
-                                FOODPED: FOODPED.value,
+                                EMAUSER: EMAUSER.value,
+                                CELUUSER: CELUUSER.value,
+                                MENPED: MENPED.value,
                                 MSGPED: MSGPED.value
                             }
                         )
@@ -52,31 +53,3 @@ formArticulo.addEventListener('submit',
         }
     }
 );
-/*validacion de letrasTEST*/
-
-function soloLetras(e) {
-    key = e.keyCode || e.which;
-    tecla = String.fromCharCode(key).toLowerCase();
-    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-    especiales = [8, 37, 39, 46];
-
-    tecla_especial = false
-    for (var i in especiales) {
-        if (key == especiales[i]) {
-            tecla_especial = true;
-            break;
-        }
-    }
-
-    if (letras.indexOf(tecla) == -1 && !tecla_especial)
-        return false;
-}
-
-function limpia() {
-    var val = document.getElementById("miInput").value;
-    var tam = val.length;
-    for (i = 0; i < tam; i++) {
-        if (!isNaN(val[i]))
-            document.getElementById("miInput").value = '';
-    }
-}
